68a78d4f90511915034bef0dc4f693e68c3dbdf5
Jay200123 <kenken.ayonon@gmail.com>
2023-01-25 10:32:43 +0700

    react commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+
+import Nav from './Nav';
+
+const App = () => (
+
+  <div className="container pb-5">
+
+      <Nav />
+
+      <br />
+
+      <h1>MERN CRUD</h1>
+
+  </div>
+
+);
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//         <h3>MERN?</h3>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
